13f9f49824482fb0f384b10ef03dddf5bc85c3e3
Chop-Chopa <ignatev2283@gmail.com>
2025-09-21 23:09:15 +0300

    Подправил модель

--- main/models.py
+++ main/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
