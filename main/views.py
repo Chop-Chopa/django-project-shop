c9b80a3b1fbf67e913f6133d5f258f5f3dd5cf7b
Chop-Chopa <ignatev2283@gmail.com>
2025-10-05 18:37:54 +0300

    Сделал фукнции для urls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    category = None
+    if category_slug:
+        category = get_object_or_404(Category, category_slug=category_slug)
+        products = Product.objects.filter(category=category)
+
+    return render(request, 'list.html', {
+        'category' : category,
+        'categories' : categories,
+        'products': products
+    })
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    related_product = Product.objects.filte(category=product .category, available=True).exclude(id=product.id)[:4]
+    return render(request, "detail.html",{
+        'product': product,
+        'related_product': related_product,
+    })
